74268f884e4f13feacf5890572c09a111591cb78
Nid silva <80358224+Ivoneide01@users.noreply.github.com>
2022-10-22 20:24:29 +0000

    Começando

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int numero, numero2, resposta;
+    numero = 8;
+    numero2 = 9;
+    resposta = (numero == numero2)? numero+numero2:numero-numero2;
+    System.out.println(resposta);
+
+  }
+}
